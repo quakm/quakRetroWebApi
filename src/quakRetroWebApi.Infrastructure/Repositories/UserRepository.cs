d87b581f58ce6f5407a3367dc4c5163aa3e2543c
quakm <wieland-f.wueller@outlook.de>
2024-10-01 15:32:01 +0200

    Some little refactoring

--- src/quakRetroWebApi.Infrastructure/Repositories/UserRepository.cs
+++ src/quakRetroWebApi.Infrastructure/Repositories/UserRepository.cs
@@ -4,6 +4,7 @@ using quakRetroWebApi.Infrastructure.Repositories.Interfaces;
 using quakRetroWebApi.Infrastructure;
 using Dapper;
 using System.Data.Common;
+using System.Text;
 
 namespace quakRetroWebApi.Infrastructure.Repositories;
 public class UserRepository : IUserRepository
@@ -22,34 +23,35 @@ public class UserRepository : IUserRepository
 
     public async Task<UserEntity?> GetUserByIdAsync(int id)
     {
-        var userColumns = string.Join(",", _userMapping.Columns
-        .Where(x => x.Key != "Id")
-        .Select(c => $"{_userMapping.Alias}.{c.Value} AS {c.Key}"));
+        var userColumns = string.Join(", ", _userMapping.Columns
+            .Where(x => x.Key != "Id")
+            .Select(c => $"{_userMapping.Alias}.{c.Value} AS {c.Key}"));
 
-        var settingsColumns = string.Join(",", _userSettingMapping.Columns
+        var settingsColumns = string.Join(", ", _userSettingMapping.Columns
             .Where(x => x.Key != "UserId")
             .Select(c => $"{_userSettingMapping.Alias}.{c.Value} AS {c.Key}"));
 
-        var query = $@"SELECT 
-                       {_userMapping.Alias}.{_userMapping.Columns["Id"]} AS Id,
-                       {userColumns},
-                       {_userSettingMapping.Alias}.{_userSettingMapping.Columns["UserId"]} AS UserId,
-                       {settingsColumns}
-        FROM {_userMapping.TableName} AS {_userMapping.Alias}
-        LEFT JOIN {_userSettingMapping.TableName} AS {_userSettingMapping.Alias} 
-            ON {_userMapping.Alias}.{_userMapping.Columns["Id"]} = {_userSettingMapping.Alias}.{_userSettingMapping.Columns["UserId"]}
-        WHERE {_userMapping.Alias}.{_userMapping.Columns["Id"]} = @Id";
+        var query = new StringBuilder();
+        query.AppendLine("SELECT");
+        query.AppendLine($"{_userMapping.Alias}.{_userMapping.Columns["Id"]} AS Id,");
+        query.AppendLine($"{userColumns},");
+        query.AppendLine($"{_userSettingMapping.Alias}.{_userSettingMapping.Columns["UserId"]} AS UserId,");
+        query.AppendLine($"{settingsColumns}");
+        query.AppendLine($"FROM {_userMapping.TableName} AS {_userMapping.Alias}");
+        query.AppendLine($"LEFT JOIN {_userSettingMapping.TableName} AS {_userSettingMapping.Alias}");
+        query.AppendLine($"ON {_userMapping.Alias}.{_userMapping.Columns["Id"]} = {_userSettingMapping.Alias}.{_userSettingMapping.Columns["UserId"]}");
+        query.AppendLine($"WHERE {_userMapping.Alias}.{_userMapping.Columns["Id"]} = @Id");
 
         using var connection = _dbContext.CreateConnection();
         var result = await connection.QueryAsync<UserEntity, UserSettingsEntity, UserEntity>(
-            query,
+            query.ToString(),
             (user, settings) =>
             {
                 user.UserSettings = settings;
                 return user;
             },
             new { Id = id },
-            splitOn: $"UserId"
+            splitOn: "UserId"
         );
 
         return result.SingleOrDefault();
